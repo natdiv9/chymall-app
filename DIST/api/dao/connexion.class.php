bc52466ffe7f49187bfe1d13c5beb49fd69d3232
Christian Mwamba <35748114+Natdiv@users.noreply.github.com>
2021-10-19 13:32:14 +0200

    Update connexion.class.php

--- DIST/api/dao/connexion.class.php
+++ DIST/api/dao/connexion.class.php
@@ -6,13 +6,13 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Authorization, C
 
 class Connexion
 {
-    public static function getConnexion($db_name = 'chymall')
+    public static function getConnexion($db_name = '')
     {
         try {
            // $host = "mysql:host=vt60918-001.privatesql;port=35851;dbname=$db_name";
             $host = "mysql:host=localhost;dbname=$db_name";
-            $user = 'root'; //'chryadmin'; //root
-            $pwd = 'LInuxDEbian09';
+            $user = ''; 
+            $pwd = '';
             $bdd = new PDO($host, $user, $pwd,
                 array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
